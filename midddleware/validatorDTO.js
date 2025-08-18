e82fa9334b285639e1864c55e085582e9700342f
Nicoleordonezc <nicoloe24@gmail.com>
2025-08-18 13:07:55 -0500

    feat: :lipstick: Created validator DTO

--- midddleware/validatorDTO.js
+++ midddleware/validatorDTO.js
@@ -1,2 +1,9 @@
 import { validationResult } from "express-validator";
 
+export const validatorDTO = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    next();
+}
